89b9ab19c351415afa8148d045e82c2bd3915cf2
Sonu Sharma <sonu.sharma045@gmail.com>
2023-03-22 15:30:49 +0530

    🚥  env ++

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,4 +1,4 @@
-const MONGO_URI = process.env.MONGO_URI ?? "mongodb://localhost:27017/pulse-db";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/pulse-db";
 
 const API_PORT = process.env.PORT || 3000;
 
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,6 +2,7 @@ const Mongoose = require("mongoose");
 const { MONGO_URI } = require("./config");
 
 exports.connect = () => {
+  console.log("🚀 ~ Env:", process.env);
   console.log("🚀 ~ Connecting Db at:", MONGO_URI);
   Mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
