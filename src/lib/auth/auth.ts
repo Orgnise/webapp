f6085872a35073af8029c2e880913d41bd05b0d9
Sonu Sharma <sonu.sharma045@gmail.com>
2024-04-14 17:11:28 +0530

    Fix asset upload url.

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -176,6 +176,7 @@ export const NextAuthOptions = {
               $set: {
                 ...(!userExists.name && { name: profile.name || profile.login }),
                 ...(newAvatar && { image: newAvatar }),
+                updatedAt: new Date().toISOString(),
               }
             },
           );
